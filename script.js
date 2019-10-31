9dff213a94e543a60057694e29450151648dc62c
Mark Spivak <msp3472@gmail.com>
2019-10-31 14:47:17 -0400

    added semicolons

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var altorfer = "Kenneth%20J.%20Altorfer%20Academic%20hoboken"
-var burchard = "burchard%20memorial%20stevens%20hoboken"
-var babbio = "babbio%20center%20hoboken"
-var carnegie = "carnegie%20lab%20hoboken"
-var cph = "castle%20point%20hall%20hoboken"
-var davis = "davis%20hall%20hoboken"
-var eas = "edwin%20a%20stevens%20hoboken"
-var gate = "gateway%20academic%20center%20hoboken"
-var howe = "howe%20building%20stevens%20hoboken"
-var humps = "humphreys%20hall%20hoboken"
-var jonas = "jonas%20hall%20hoboken"
-var mclean = "mclean%20hoboken"
-var mpk = "morton%20pierce%20hoboken"
-var north = "north%20building%20stevens%20hoboken"
-var palmer = "palmer%20hall%20hoboken"
+var altorfer = "Kenneth%20J.%20Altorfer%20Academic%20hoboken";
+var burchard = "burchard%20memorial%20stevens%20hoboken";
+var babbio = "babbio%20center%20hoboken";
+var carnegie = "carnegie%20lab%20hoboken";
+var cph = "castle%20point%20hall%20hoboken";
+var davis = "davis%20hall%20hoboken";
+var eas = "edwin%20a%20stevens%20hoboken";
+var gate = "gateway%20academic%20center%20hoboken";
+var howe = "howe%20building%20stevens%20hoboken";
+var humps = "humphreys%20hall%20hoboken";
+var jonas = "jonas%20hall%20hoboken";
+var mclean = "mclean%20hoboken";
+var mpk = "morton%20pierce%20hoboken";
+var north = "north%20building%20stevens%20hoboken";
+var palmer = "palmer%20hall%20hoboken";
 
 function showDirections(loc) {
-    document.getElementById("gmap_canvas").src = "https://maps.google.com/maps?q="+ loc + "&t=&z=13&ie=UTF8&iwloc=&output=embed"
+    document.getElementById("gmap_canvas").src = "https://maps.google.com/maps?q="+ loc + "&t=&z=13&ie=UTF8&iwloc=&output=embed";
 }
