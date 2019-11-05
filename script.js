9be23a9cde17802cce4313e0c0f5cd1ec9dcfe39
nparekh13011 <nparekh2@stevens.edu>
2019-11-05 15:17:37 -0500

    Added Places Outside of Campus

--- script.js
+++ script.js
@@ -13,6 +13,20 @@ var mclean = 'mclean%20hoboken';
 var mpk = 'morton%20pierce%20hoboken';
 var north = 'north%20building%20stevens%20hoboken';
 var palmer = 'palmer%20hall%20hoboken';
+var schaefer= 'schaefer%20athletic%20center%hoboken';
+var bennys = 'bennys%20tudinos%20hoboken';
+var seveneleven ='7%20Eleven%20hoboken'
+var CVS = 'CVS%20Pharmacy%20hoboken';
+var starbucks = 'starbucks%20hoboken';
+var Chipotle = 'chipotle%20hoboken';
+var insomnia = 'insomnia%20cookies%20hoboken';
+var pitapit = 'pita%20pit%20hoboken';
+var shoprite = 'shoprite%20hoboken';
+var Kungfutea = 'Kung%20Fu%20hoboken';
+var Walgreens = 'Walgreens%20hoboken';
+var karmakafe = 'Karma%20Kafe%20hoboken';
+var Simplychai = 'Simply%20Chai%20hoboken';
+
 
 function showDirections(loc) {
 	document.getElementById('gmap_canvas').src =
